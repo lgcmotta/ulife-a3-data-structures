d2d48fd4be7c322c20c86a27ad11225944b35aa6
lgcmotta <lgcmotta01@gmail.com>
2025-11-19 23:56:24 -0300

    feat: implement Program entrypoint with Cocona

--- src/A3.RouteSearchGraphs/Program.cs
+++ src/A3.RouteSearchGraphs/Program.cs
@@ -1,1 +1,46 @@
-Console.WriteLine();
+using System.Runtime.Loader;
+using A3.RouteSearchGraphs.Commands;
+using A3.RouteSearchGraphs.Extensions;
+using Cocona;
+
+CancellationTokenSource cts = new();
+
+Console.CancelKeyPress += HandleCancelKeyPress;
+AppDomain.CurrentDomain.ProcessExit += HandleCancelKeyPress;
+AssemblyLoadContext.Default.Unloading += HandleUnloading;
+
+var builder = CoconaApp.CreateBuilder(args);
+
+builder.Logging.AddConsole();
+builder.Services.AddAnsiConsole();
+builder.Services.AddReportLocalization();
+
+var app = builder.Build();
+
+app.AddRunCommand();
+
+try
+{
+    await app.RunAsync();
+}
+catch (OperationCanceledException)
+{
+    // Application is shutting down with SIGTERM or SIGINT
+}
+
+return;
+
+void HandleCancelKeyPress(object? source, EventArgs args)
+{
+    if (args is ConsoleCancelEventArgs eventArgs)
+    {
+        eventArgs.Cancel = true;
+    }
+
+    cts.Cancel();
+}
+
+void HandleUnloading(AssemblyLoadContext _)
+{
+    cts.Cancel();
+}
